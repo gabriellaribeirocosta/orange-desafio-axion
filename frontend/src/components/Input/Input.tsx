27891c0fc7715dce29b220b56e88839406d49a03
Gabriella Costa <gabx2205@gmail.com>
2025-02-08 20:31:14 -0300

    feat: add input component

---
+++ frontend/src/components/Input/Input.tsx
@@ -0,0 +1,32 @@
+import styles from './Input.module.css'
+import emailIcon from '@/assets/mail.png'
+import lockIcon from '@/assets/lock.png'
+import Image from 'next/image'
+
+interface InputProps {
+    type?: 'text' | 'email' | 'password'
+    placeholder: string
+    label: string
+    icon?: 'email' | 'password'
+}
+
+export default function Input({ type = 'text', placeholder, label, icon}:InputProps) {
+    const getIcon = () => {
+        switch(icon){
+            case 'email':
+                return emailIcon;
+            case 'password':
+                return lockIcon;
+            default:
+                return emailIcon;
+        }
+    }
+
+    return (
+      <div>
+        <label className={styles.label}>{label}</label>
+        <input type={type} className={styles.input} placeholder={placeholder}/>
+        {icon && (<div className={styles.icon}><Image alt='icon' src={getIcon()} height={18}></Image></div>)}
+      </div>
+    );
+}
